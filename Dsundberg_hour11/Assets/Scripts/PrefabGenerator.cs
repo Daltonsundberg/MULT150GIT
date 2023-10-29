a221c11437ee2a260b202cc9be4c425477bd9960
DaltonSundberg <daltonsundberg@me.com>
2023-10-28 22:36:58 -0400

    Added Line Script

--- Dsundberg_hour11/Assets/Scripts/PrefabGenerator.cs
+++ Dsundberg_hour11/Assets/Scripts/PrefabGenerator.cs
@@ -1,20 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PrefabGenerator : MonoBehaviour
 {
-	public GameObject prefab;
+    public GameObject lampPrefab;
+    public float lampSpacing = 2.0f; 
 
-	void Update()
-	{
-		
-		if (Input.GetKeyDown(KeyCode.B))
-		{
-			Instantiate(prefab);
-		}
+    void Start()
+    {
+        Vector3 spawnPosition = transform.position; 
+        for (int i = 0; i < 10; i++)
+        {
+            
+            GameObject lamp = Instantiate(lampPrefab, spawnPosition, Quaternion.identity);
 
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Instantiate(prefab, transform.position, transform.rotation);
-		}
-	}
+            
+            lamp.transform.parent = transform;
+
+         
+            spawnPosition.x += lampSpacing;
+        }
+    }
+
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Instantiate(lampPrefab);
+        }
+
+       
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(lampPrefab, transform.position, transform.rotation);
+        }
+    }
 }
