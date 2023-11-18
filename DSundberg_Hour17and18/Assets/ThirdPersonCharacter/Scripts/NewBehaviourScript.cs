643d8eb252116ce40b6d722ab335727bd3059440
DaltonSundberg <daltonsundberg@me.com>
2023-11-17 22:20:59 -0500

    .

--- DSundberg_Hour17and18/Assets/ThirdPersonCharacter/Scripts/NewBehaviourScript.cs
+++ DSundberg_Hour17and18/Assets/ThirdPersonCharacter/Scripts/NewBehaviourScript.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Reference to the Animator component
+
     private Animator animator;
 
-    // Define the key you want to use as a trigger
+  
     public KeyCode triggerKey = KeyCode.Space;
 
     void Start()
